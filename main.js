fc1b26f066e664a888161ae5c97989d3081e5775
VinodhRaja Muralidharan <vinodhraja.muralidharan@cognizant.com>
2017-03-13 16:44:07 +0000

    creating server

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log('Hello Node!... I am apporaching to learn you completely!');
+
+var http = require('http');
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello.... I am the server');
+}).listen(7777);
+console.log('Server running at http://127.0.0.1:8081/');
